6b40550713227dbc6a903f013afdd0624f8695af
Pablo Henrique <pablo_hen@hotmail.com>
2021-06-14 14:36:34 -0300

    atualizacao com dados que estavam faltando

--- utils/separaDadosNoticia.js
+++ utils/separaDadosNoticia.js
@@ -1,21 +1,29 @@
+import formataData from './formataData';
+
 export const separaDadosNoticia = (noticia) => {
   const key = noticia.uid;
   const titulo = noticia.data.titulo;
   const subtitulo = noticia.data.subtitulo;
+  const corpo = noticia.data.corpo;
   const link = noticia.uid;
   const imagemUrl = noticia.data?.imagem?.url;
   const imagemLargura = noticia.data?.imagem?.dimensions?.width;
   const imagemAltura = noticia.data?.imagem?.dimensions?.height;
   const categorias = noticia.tags;
+  const dataPublicacao = formataData.padrao(noticia.first_publication_date);
+  const dataAtualizacao = formataData.padrao(noticia.last_publication_date);
 
   return {
     key,
     titulo,
     subtitulo,
+    corpo,
     link,
     imagemUrl,
     imagemLargura,
     imagemAltura,
     categorias,
+    dataPublicacao,
+    dataAtualizacao,
   };
 };
