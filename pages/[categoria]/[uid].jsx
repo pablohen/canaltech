0ab60e9448cc6b1449835d3040618c279a9690be
Pablo Henrique <pablo_hen@hotmail.com>
2021-06-14 15:37:50 -0300

    teste copiando tudo da documentação

--- pages/[categoria]/[uid].jsx
+++ pages/[categoria]/[uid].jsx
@@ -32,7 +32,26 @@ const PaginaNoticia = ({ noticia }) => {
       <NextSeo
         title={titulo}
         description={descricao}
-        openGraph={{ openGraph }}
+        openGraph={{
+          type: 'website',
+          url: 'https://www.example.com/page',
+          title: 'Open Graph Title',
+          description: 'Open Graph Description',
+          images: [
+            {
+              url: 'https://www.example.ie/og-image.jpg',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+            },
+            {
+              url: 'https://www.example.ie/og-image-2.jpg',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt 2',
+            },
+          ],
+        }}
       />
 
       <MenuPrincipal />
